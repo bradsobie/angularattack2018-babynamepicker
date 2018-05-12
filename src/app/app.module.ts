ae254f7dc853d168b83694b9ed26045cfcb46622
Brad Sobie <brad_sobie@volusion.com>
2018-05-11 23:26:32 -0500

    Fixing Kinvey initialization, and fetching data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Kinvey } from 'kinvey-angular2-sdk';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -17,4 +18,35 @@ import { SelectGenderModule } from './pages/selectGender/selectGender.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    const activeUser = Kinvey.User.getActiveUser();
+
+    if (activeUser) {
+      activeUser.me()
+        .then((activeUser: Kinvey.User) => {
+          this.getData();
+        });
+    } else {
+      Kinvey.User.signup()
+        .then((user: Kinvey.User) => {
+          this.getData();
+        });
+    }
+  }
+
+  getData() {
+    const dataStore = Kinvey.DataStore.collection('names');
+    const query = new Kinvey.Query();
+    query.greaterThanOrEqualTo('count', 100);
+
+    dataStore.find(query)
+      .subscribe((entities: {}[]) => {
+        console.log(entities);
+      }, (error: Kinvey.BaseError) => {
+        console.log(error);
+      }, () => {
+        console.log('complete');
+      });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,18 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 const config: Kinvey.ClientConfig = {
-  appKey: 'kid_SkMpnRmAz',
+  appKey: '<appKey>',
   appSecret: '1b685d17d5a347a68455741e3a5ab3a4'
 };
 
-Kinvey.initialize(config)
-  .then(() => {
-    if (environment.production) {
-      enableProdMode();
-    }
-    platformBrowserDynamic().bootstrapModule(AppModule)
-      .catch(err => console.log(err));
-  });
+if (environment.production) {
+  enableProdMode();
+}
+
+Kinvey.init({
+  appKey: 'kid_SkMpnRmAz',
+  appSecret: '1b685d17d5a347a68455741e3a5ab3a4'
+});
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));

419293fff7fc6b5952217b47a57682848b04737b
Brad Sobie <brad_sobie@volusion.com>
2018-05-11 22:32:51 -0500

    Adding Kinvey

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Kinvey } from 'kinvey-angular2-sdk';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+const config: Kinvey.ClientConfig = {
+  appKey: 'kid_SkMpnRmAz',
+  appSecret: '1b685d17d5a347a68455741e3a5ab3a4'
+};
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+Kinvey.initialize(config)
+  .then(() => {
+    if (environment.production) {
+      enableProdMode();
+    }
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.log(err));
+  });

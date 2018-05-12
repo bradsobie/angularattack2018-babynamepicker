2c3eb9db6ba58415425303b568e12f1b1bf819c7
Brad Sobie <brad_sobie@volusion.com>
2018-05-12 14:53:46 -0500

    Adding spinner when fetching names

--- src/app/components/namePicker/namePicker.module.ts
+++ src/app/components/namePicker/namePicker.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NamePickerComponent } from './namePicker.component';
 import { NameCardComponent } from './nameCard.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     NamePickerComponent,

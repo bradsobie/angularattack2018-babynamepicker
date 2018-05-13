d9a51faf9594f981b22eb944a48c256d2e1654c7
Brad Sobie <brad_sobie@volusion.com>
2018-05-13 06:04:45 -0500

    Adding swipe events

--- src/app/components/namePicker/namePicker.component.ts
+++ src/app/components/namePicker/namePicker.component.ts
@@ -16,6 +16,8 @@ export class NamePickerComponent implements OnInit {
   gender: any;
   whitelist: any;
   blacklist: any;
+  swipeCoord: any;
+  swipeTime: any;
   constructor(
     private namesService: NamesService,
     private userService: UserService,
@@ -67,5 +69,29 @@ export class NamePickerComponent implements OnInit {
         this.blacklist = newBlacklist;
         this.showNewName()
       });
+  }
+
+  swipe(e, when) {
+    const coord = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    }
+
+    else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 &&
+          Math.abs(direction[0]) > 30 &&
+          Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+        const swipeDirection = direction[0] < 0 ? 'next' : 'previous';
+        
+        if (swipeDirection === 'next') {
+          this.showNewName();
+        }
+    }
   }
 }
